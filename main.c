20258c541f7fef6ac5ef0622c0d81649ee263b42
Noel S Cruz <65864827+noey2020@users.noreply.github.com>
2020-07-01 18:06:45 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "stm32l1xx.h"
+
+volatile uint32_t msTicks = 0;        /* Variable to store millisecond ticks */
+
+void SysTick_Handler(void){           /* SysTick interrupt Handler. */
+    msTicks++;                        /* See startup file startup_LPC17xx.s for SysTick vector */
+}
+
+int main(void){
+    uint32_t returnCode;
+
+    returnCode = SysTick_Config(SystemCoreClock / 1000);    /* Configure SysTick to generate an interrupt every millisecond */
+
+    if(returnCode != 0){                                    /* Check return code for errors */
+    // Error Handling
+    }
+    while(1);
+}
